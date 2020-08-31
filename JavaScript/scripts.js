c3a062cfcc996b5b055188a290756fc5feb4a1c4
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-08-31 16:12:28 -0400

    added jquery hide/show functions to the What we do section

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,7 +1,28 @@
 $(document).ready(function () {
-  $("button").click(function () {
-    $("design-icon").hide();
+  $("#design-icon").click(function () {
+    $("#design-icon").hide();
     $("#design-paragraph").show();
   });
-  $("#design-paragraph").click(function () {});
+  $("#design-paragraph").click(function () {
+    $("#design-icon").show();
+    $("#design-paragraph").hide();
+  });
+
+  $("#development-icon").click(function () {
+    $("#development-icon").hide();
+    $("#development-paragraph").show();
+  });
+  $("#development-paragraph").click(function () {
+    $("#development-icon").show();
+    $("#development-paragraph").hide();
+  });
+
+  $("#product-icon").click(function () {
+    $("#product-icon").hide();
+    $("#product-paragraph").show();
+  });
+  $("#product-paragraph").click(function () {
+    $("#product-icon").show();
+    $("#product-paragraph").hide();
+  });
 });
