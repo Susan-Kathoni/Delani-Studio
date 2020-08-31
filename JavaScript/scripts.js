2754a561f2b151384a9d71485db478a69998eb4f
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-08-31 18:04:09 -0400

    adding hover effects over portfolio section images

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -26,3 +26,66 @@ $(document).ready(function () {
     $("#product-paragraph").hide();
   });
 });
+
+$(document).ready(function(){
+$(".img-fluid").mouseover(function () {
+  $("#Portfolio-img1").show();
+});
+$("#img-fluid").mouseout(function () {
+  $("#Portfolio-img1").hide();
+});
+$(".img-fluid").mouseover(function () {
+  $(".#Portfolio-img2").show();
+});
+$(".img-fluid").mouseout(function () {
+  $("#Portfolio-img2").hide();
+});
+$(".img-fluid").mouseover(function () {
+  $("#Portfolio-img3").show();
+});
+$(".img-fluid").mouseout(function () {
+  $("#Portfolio-img3").hide();
+});
+$(".img-fluid").mouseover(function () {
+  $("#Portfolio-img4").show();
+});
+$(".img-fluid").mouseout(function () {
+  $(".#Portfolio-img4").hide();
+});
+$(".img-fluid").mouseover(function () {
+  $("#Portfolio-img5").show();
+});
+$(".img-fluid").mouseout(function () {
+  $("#Portfolio-img5").hide();
+});
+$(".img-fluid").mouseover(function () {
+  $("#Portfolio-img6").show();
+});
+$(".img-fluid").mouseout(function () {
+  $("#Portfolio-img6").hide();
+});
+$(".img-fluid").mouseover(function () {
+  $("#Portfolio-img7").show();
+});
+$(".img-fluid").mouseout(function () {
+  $("#Portfolio-img7").hide();
+});
+$(".img-fluid").mouseover(function () {
+  $("#Portfolio-img8").show();
+});
+$(".img-fluid").mouseout(function () {
+  $("#Portfolio-img8").hide();
+});
+
+//form function
+function contact(form) {
+  var name = document.forms["contacts"]["name"].value;
+  var email = document.forms["contacts"]["email"].value;
+  var message = document.forms["contacts"]["message"].value;
+  alert(
+    "Hello! " +
+      name +
+      "," +
+      " We have received your message. Thank you for reaching out to us. "
+  );
+}
