324e3fa5d23e2dc04fab719e1b77bb99dbe32638
Susan-Kathoni <123joey4kathoniey@gmail.com>
2020-09-03 09:45:44 -0400

    changed the alert message

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -76,62 +76,15 @@ $(document).ready(function () {
   $(".img7").mouseout(function () {
     $(".work7").hide();
   });
-});
 
-//form function
-function feedback(form) {
-  var name = document.forms["feedback"]["name"].value;
-  var email = document.forms["feedback"]["email"].value;
-  var message = document.forms["feedback"]["message"].value;
-  alert(
-    "Hello! " +
-      name +
-      "," +
-      " Your message has been received. Thanks for your feedback. "
-  );
-}
-$(function () {
-  // Get the form.
-  var form = $('#ajax-contact');
-
-  // Get the messages div.
-  var formMessages = $('#form-messages');
-  // Set up an event listener for the contact form.
-  $(form).submit(function (event) {
-    // Stop the browser from submitting the form.
-    event.preventDefault();
-    var formData = $(form).serialize();
-  // Submit the form using AJAX.
-$.ajax({
-  type: 'POST',
-  url: $(form).attr('action'),
-  data: formData
-})
-.done(function(response) {
-  // Make sure that the formMessages div has the 'success' class.
-  $(formMessages).removeClass('error');
-  $(formMessages).addClass('success');
-
-  // Set the message text.
-  $(formMessages).text(response);
-
-  // Clear the form.
-  $('#name').val('');
-  $('#email').val('');
-  $('#message').val('');
-})
-.fail(function(data) {
-  // Make sure that the formMessages div has the 'error' class.
-  $(formMessages).removeClass('success');
-  $(formMessages).addClass('error');
-
-  // Set the message text.
-  if (data.responseText !== '') {
-      $(formMessages).text(data.responseText);
-  } else {
-      $(formMessages).text('Oops! An error occured and your message could not be sent.');
+  //form function
+  function feedback(form) {
+    var name = document.forms["feedback"]["name"].value;
+    var email = document.forms["feedback"]["email"].value;
+    var message = document.forms["feedback"]["message"].value;
+    alert(
+      " Your subscription to our list has been confirmed.Thank you for subscribing! "
+    );
   }
 });
-  
-  });
-});
+
